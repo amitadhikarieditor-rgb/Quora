dc4ce5be1ba3d59f4944dbd4c2881fb347b2d5e6
Prateek Adhikari <prateekadhikari@prateeks-MacBook-Pro.local>
2026-08-02 13:04:45 +0530

    added the functionality of edit and delete

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require("express");
 const app=express();
 const path=require("path");
 const {v4:uuid}=require("uuid");
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 
 const port=8080;
@@ -30,10 +32,6 @@ let posts =[
         content:"3 FIR "
     },
 ]
-app.listen(port,(req,res)=>{
-    console.log(`listening to the port: ${port}`)
-});
-
 app.get("/posts",(req,res)=>{
     res.render("index.ejs",{posts});
 });
@@ -51,4 +49,27 @@ app.get("/posts/:id",(req,res)=>{
     let {id}=req.params;
     let post=posts.find((post)=>post.id===id);
     res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+    let post=posts.find((post)=>post.id===id);
+    post.content=req.body.content;
+    res.redirect("/posts");
+});
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>p.id===id);
+    res.render("edit.ejs",{post});
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts =posts.filter((p)=> p.id !==id)
+    res.redirect("/posts");
+});
+
+
+app.listen(port,(req,res)=>{
+    console.log(`listening to the port: ${port}`)
 });
